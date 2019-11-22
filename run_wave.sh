26f52eae77eafa2f16083ca598856ef075dd72d3
Josh Rule <joshua.s.rule@gmail.com>
2019-11-22 20:27:58 +0000

    more debugging for failed runs

--- run_wave.sh
+++ run_wave.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find trs/waves/$1 -maxdepth 1 -mindepth 1 -type d | parallel --progress --bar "RUST_BACKTRACE=1 cargo run --release --bin simulation {}/simulation.toml &> {}/`date '+%Y-%m-%d-%H-%M-%S'`.log"
+find trs/waves/$1 -maxdepth 1 -mindepth 1 -type d | parallel --progress --bar "RUST_BACKTRACE=full cargo run --release --bin simulation {}/simulation.toml &> {}/`date '+%Y-%m-%d-%H-%M-%S'`.log"
