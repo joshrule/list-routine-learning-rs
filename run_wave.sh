50ac781f28c36765b9032fac53f8ea3e3e1581e1
Josh Rule <joshua.s.rule@gmail.com>
2019-12-27 16:34:34 +0000

    rerun Wave 1.5

--- run_wave.sh
+++ run_wave.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# $1 = wave_dir
+# $2 = out_dir
+# $3 = # of runs
+
 HERE=`pwd`
-parallel --progress --bar "RUST_BACKTRACE=full cargo run --release --bin simulation trs/waves/$1/{1}/simulation.toml &> $2/{1}_{2}.log" ::: $(cd trs/waves/$1 && find -maxdepth 1 -mindepth 1 -type d -printf %f\\n && cd $HERE) ::: $(seq -w 0 `expr $3 - 1`)
+parallel --joblog $2/log.job --jobs=34 "RUST_BACKTRACE=full cargo run --release --bin simulation trs/waves/$1/{1}/simulation.toml &> $2/{1}_{2}.log" ::: $(cd trs/waves/$1 && find -maxdepth 1 -mindepth 1 -type d -printf %f\\n && cd $HERE) ::: $(seq -w 0 `expr $3 - 1`)
