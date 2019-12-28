ab0af6943f363128136923384e083ef09b1a3518
Josh Rule <joshua.s.rule@gmail.com>
2019-12-28 19:33:52 +0000

    add usage, variable job no. to run_wave.sh

--- run_wave.sh
+++ run_wave.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# $1 = wave_dir
-# $2 = out_dir
-# $3 = # of runs
-
+[ $# -le 3 ] && { echo "Usage: $0 <prob_dir> <out_dir> <# runs> <# jobs>"; exit 1; }
 HERE=`pwd`
-parallel --joblog $2/log.job --jobs=34 "RUST_BACKTRACE=full cargo run --release --bin simulation trs/waves/$1/{1}/simulation.toml &> $2/{1}_{2}.log" ::: $(cd trs/waves/$1 && find -maxdepth 1 -mindepth 1 -type d -printf %f\\n && cd $HERE) ::: $(seq -w 0 `expr $3 - 1`)
+parallel --joblog $2/log.job --jobs=$4 "RUST_BACKTRACE=full cargo run --release --bin simulation trs/waves/$1/{1}/simulation.toml &> $2/{1}_{2}.log" ::: $(cd trs/waves/$1 && find -maxdepth 1 -mindepth 1 -type d -printf %f\\n && cd $HERE) ::: $(seq -w 0 `expr $3 - 1`)
