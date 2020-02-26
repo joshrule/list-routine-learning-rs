ac42671f412c68e13b12971341da713aa29cc551
Josh Rule <joshua.s.rule@gmail.com>
2020-02-26 11:03:34 -0500

    run_wave.sh: use a single simulation file for all runs

--- run_wave.sh
+++ run_wave.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-[ $# -le 3 ] && { echo "Usage: $0 <prob_dir> <out_dir> <# runs> <# jobs>"; exit 1; }
-HERE=`pwd`
-parallel --joblog $2/log.job --jobs=$4 "RUST_BACKTRACE=full cargo run --release --bin simulation trs/waves/$1/{1}/simulation.toml &> $2/{1}_{2}.log" ::: $(cd trs/waves/$1 && find -maxdepth 1 -mindepth 1 -type d -printf %f\\n && cd $HERE) ::: $(seq -w 0 `expr $3 - 1`)
+[ $# -le 4 ] && { echo "Usage: $0 <prob_dir> <sim_file> <out_dir> <# runs> <# jobs>"; exit 1; }
+parallel --joblog $3/log.job --jobs=$5 "RUST_BACKTRACE=full cargo run --release --bin simulation -- $2 $1/{1} &> $3/{1}_{2}.log" ::: $(find $1 -maxdepth 1 -mindepth 1 -type d -printf %f\\n) ::: $(seq -w 0 `expr $4 - 1`)
