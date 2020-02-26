994081c5d0caa24b03c342c5aa504ec434edbc8e
Josh Rule <joshua.s.rule@gmail.com>
2020-02-26 12:42:07 -0500

    run_wave.sh: overhaul of run_wave.sh

--- run_wave.sh
+++ run_wave.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
 
-[ $# -le 4 ] && { echo "Usage: $0 <prob_dir> <sim_file> <out_dir> <# runs> <# jobs>"; exit 1; }
-parallel --joblog $3/log.job --jobs=$5 "RUST_BACKTRACE=full cargo run --release --bin simulation -- $2 $1/{1} &> $3/{1}_{2}.log" ::: $(find $1 -maxdepth 1 -mindepth 1 -type d -printf %f\\n) ::: $(seq -w 0 `expr $4 - 1`)
+# This is a script for running simulations.
+
+# Give it a parameter file, problem file, number of runs, and number of jobs:
+# - parameter_file is a TOML file containing all the parameters of the simulation.
+# - problem_file is a plain text list of the directoriescontaining each problem to be run.
+# - num_runs is an integer, the number of runs to complete for each problem.
+# - num_jobs is an integer, the number of jobs to run in parallel.
+# - description is a quoted string. What test are you running and why?
+[ $# -ne 5 ] && { echo "Usage: $0 <simulation_file> <problem_file> <num_runs> <num_jobs> <description>"; exit 1; }
+
+# Create a timestamped directory.
+DIRNAME=`date "+%s_%Y-%m-%d-%H-%M-%S"`
+OUTDIR=out/$DIRNAME
+mkdir $OUTDIR
+
+# Copy the parameter file and problem file into the directory.
+cp $1 $OUTDIR/parameters.toml
+cp $2 $OUTDIR/problems.txt
+
+# Update a generic readme and test-specific readme and csv file.
+echo "- [\`$DIRNAME\`]($DIRNAME/readme.md): $5" >> out/readme.md
+
+echo "# \`$DIRNAME\`" >> $OUTDIR/readme.md
+echo "" >> $OUTDIR/readme.md
+echo "$5" >> $OUTDIR/readme.md
+echo "- [\`parameter file\`](./parameters.toml)" >> $OUTDIR/readme.md
+echo "- [\`problem file\`](./problems.txt)" >> $OUTDIR/readme.md
+echo "- number of runs: $3" >> $OUTDIR/readme.md
+echo "- number of jobs: $4" >> $OUTDIR/readme.md
+
+[ ! -e out/simulations.csv ] && echo "directory,parameter_file,problem_file,num_runs,num_jobs,reason" >> out/simulations.csv
+echo "\"$DIRNAME\",\"$DIRNAME/parameters.toml\",\"$DIRNAME/problems.txt\",$3,$4,\"$5\"" >> out/simulations.csv
+
+# Run the tests.
+parallel --joblog $OUTDIR/job.txt --jobs=$4 "RUST_BACKTRACE=full cargo run --release --bin simulation -- $1 {1} &> $OUTDIR/{=1 s:\/:\.:g; =}_{2}.log" ::: $(cat $2) ::: $(seq -w 0 `expr $3 - 1`)
